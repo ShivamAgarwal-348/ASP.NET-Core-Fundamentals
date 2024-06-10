3dadd3e1b918a32ad4a0f328e4d0fd19eca9d168
Shivam Agarwal <Agarwal.Shivam348@gmail.com>
2024-06-10 22:30:56 +0530

    Add project files.

---
+++ OdeToFood/Pages/Restaurants/File.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class FileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
